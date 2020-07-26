0b4d314237054e209c934278c364e87968363061
Amany Emam <59666598+AmanyEmam@users.noreply.github.com>
2020-07-26 13:48:41 -0400

    Create plot 4.R

---
+++ plot 4.R
@@ -0,0 +1,32 @@
+
+##plot 4
+
+Householdpower <- read.table("household_power_consumption.txt",skip=1,sep=";")
+names(Householdpower) <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
+subpower <- subset(Householdpower,Householdpower$Date=="1/2/2007" | Householdpower$Date =="2/2/2007")
+
+# Transform the Date and Time vars from characters into objects of type Date and POSIXlt respectively
+subpower$Date <- as.Date(subpower$Date, format="%d/%m/%Y")
+subpower$Time <- strptime(subpower$Time, format="%H:%M:%S")
+subpower[1:1440,"Time"] <- format(subpower[1:1440,"Time"],"2007-02-01 %H:%M:%S")
+subpower[1441:2880,"Time"] <- format(subpower[1441:2880,"Time"],"2007-02-02 %H:%M:%S")
+
+par(mfrow = c(2,2))         
+with(subpower, {
+  plot(Time, as.numeric(as.character(subpower$Global_active_power)), type ="l", ylab="Global Active Power",xlab="")   
+  plot(Time, Voltage, type = "l", xlab = "datetime", ylab = "Volatge")
+  
+  plot(subpower$Time, subpower$Sub_metering_1, type = "n", xlab = "", ylab = "Energy sub metering", main="Energy sub-metering")
+  with (subpower, lines(Time, as.numeric(as.character(Sub_metering_1))))
+  with (subpower, lines(Time, as.numeric(as.character(Sub_metering_2)), col="red"))
+  with (subpower, lines(Time, as.numeric(as.character(Sub_metering_3)), col="blue"))
+  legend("topright", lty = 1, col = c("black","red","blue"), legend = c("sub_metering_1","sub_metering_2","sub_metering_3"))
+  
+  
+  with(subpower, plot(Time, as.numeric(as.character(Global_reactive_power)), type="l", xlab="datetime", ylab="Global_reactive_power"))
+  
+  
+})
+
+dev.copy(png, file = "plot4.png", width = 480, height = 480, units = "px")
+dev.off()
